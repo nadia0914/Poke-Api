1bb6dce9c64f9e304bc3c4e972d455a57c67cbd9
Nadia <lizbeth.m.salto@gmail.com>
2023-12-20 23:29:44 -0600

    Primer Commit

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// service-worker.js
+
+const CACHE_NAME = 'my-pwa-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js'
+  // Agrega aquí todos los archivos de tu aplicación que deseas cachear
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
