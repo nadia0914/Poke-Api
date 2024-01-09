2455935b409d84df13ee1f77bd8d72a7746bc744
Nadia <lizbeth.m.salto@gmail.com>
2024-01-08 20:28:35 -0600

    eighth Commit

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,6 @@
-const CACHE ='cache-1';
+const CACHE ='cache-poke-api';
 const CACHE_DINAMICO ='dinamico-1';
 const CACHE_INMUTABLE ='inmutable-1';
-
-//declaración de constante para poner todos los archivos requeridos por el APP SHELL
-const APP_SHELL = [
-    '/',
-    'index.html'
-];
-
 
 //Indicamos que durante el proceso de intalación se 
 //carguen los archivos del cache estatico
@@ -15,20 +8,22 @@ self.addEventListener('install', evento=>{
     /*Promesa que crea el proceso de creación del espacio 
     en cache y agrega los archivos necesarios para cargar nuestra
     aplicación*/
-    const cacheEstatico = caches.open(CACHE)
+    const promesa =caches.open(CACHE)
         .then(cache=>{
-            cache.addAll(APP_SHELL);
-        });
-    const cacheInmutable = caches.open(CACHE_INMUTABLE)
-        .then(cache =>{
-            cache.addAll(APP_SHELL_INMUTABLE);
-        });
+            return cache.addAll([
+               // '/',
+                'index.html',
+                'offline.html'
 
-    //Indicamos que la instalación espere hasta que las promesas se cumplan
-    evento.waitUntil(Promise.all([cacheEstatico, cacheInmutable]));
+            ]);
+        });
+ 
+        //Separamos los archivos que no se modificarán en un espacio de cache inmutable
+ 
+       
+            //Indicamos que la instalación espere hasta que las promesas se cumplan
+        evento.waitUntil(Promise.all([promesa, cacheInmutable]));
 });
-
-
 //Indicamos que durante el proceso de activación se borren los
 //espacios de cache estatico de versiones pasadas
 self.addEventListener('activate', evento =>{
@@ -41,48 +36,64 @@ self.addEventListener('activate', evento =>{
                 //borralo, la condición de include cache evitará borrar el espacio dinamico o inmutable
                 return caches.delete(key);
             }
-
-            //si el espacio no tiene el nombre actual del cache e incluye la palabra dinamico
-            if(key !== CACHE_DINAMICO && key.includes('dinamico')){
-                //borralo, la condición de include cache evitará borrar el espacio dinamico o inmutable
-                return caches.delete(key);
-            }
         });
     });
     evento.waitUntil(respuesta);
 });
 //Atrapamos las peticiones y las procesamos con alguna estrategia de cache
 self.addEventListener('fetch', evento =>{
-    
-    let respuesta;
-    if(evento.request.url.includes('db') || evento.request.method === 'POST'){
-        respuesta = manejoMensajes(CACHE_DINAMICO, evento.request);
-    } else{
-        //Estrategia 2 CACHE WITH NETWORK FALLBACK
-        respuesta=caches.match(evento.request)
-            .then(res=>{
-                //si el archivo existe en cache retornalo
-                if (res){
-                    actualizaCacheStatico( CACHE, evento.request, APP_SHELL_INMUTABLE );
-                    return res;
-                } else{
-                    //Procesamos la respuesta a la petición localizada en la web
-                    return fetch(evento.request)
-                        .then(resWeb=>{
-                            //el archivo recuperado se almacena en resWeb
-                            //se abre nuestro cache
-                            return actualizaCacheDinamico( CACHE_DINAMICO, evento.request, resWeb );
-                        });
-                    }
-            })
-            .catch(err => {
-                //si ocurre un error, en nuestro caso no hay conexión
-                if(evento.request.headers.get('accept').includes('text/html')){
-                    //si lo que se pide es un archivo html muestra nuestra página offline que esta en cache
-                    return caches.match('offline.html');
-                }
-            });
-    }
 
+    
+    //Estrategia 2 CACHE WITH NETWORK FALLBACK
+    const respuesta=caches.match(evento.request)
+        .then(res=>{
+            //si el archivo existe en cache retornalo
+            if (res) return res;
+
+        
+            //Procesamos la respuesta a la petición localizada en la web
+            return fetch(evento.request)
+                .then(resWeb=>{//el archivo recuperado se almacena en resWeb
+                    //se abre nuestro cache
+                    caches.open(CACHE_DINAMICO)
+                        .then(cache=>{
+                            //se sube el archivo descargado de la web
+                            cache.put(evento.request,resWeb);
+                            //Mandamos llamar la limpieza al cargar un nuevo archivo
+                            //estamos indicando que se limpiará el cache dinamico y que 
+                            //solo debe haber 2 archivos
+                            limpiarCache(CACHE_DINAMICO,2);
+                        })
+                    //se retorna el archivo recuperado para visualizar la página
+                    return resWeb.clone();  
+                });
+        })
+        .catch(err => {
+            //si ocurre un error, en nuestro caso no hay conexión
+            if(evento.request.headers.get('accept').includes('text/html')){
+                //si lo que se pide es un archivo html muestra nuestra página offline que esta en cache
+                return caches.match('/offline.html');
+            }
+        });
         evento.respondWith(respuesta);
+        
+
 });
+
+//recibimos el nombre del espacio de c ache a limpiar y el número de archivos permitido
+function limpiarCache(nombreCache, numeroItems){
+    //abrimos el cache
+    caches.open(nombreCache)
+        .then(cache=>{
+            //recuperamos el arreglo de archivos exixtentes en el espacio de cache
+            return cache.keys()
+                .then(keys=>{
+                    //si el número de archivos supera el limite permitido
+                    if (keys.length>numeroItems){
+                        //eliminamos el más antigüo y repetimos el proceso
+                        cache.delete(keys[0])
+                            .then(limpiarCache(nombreCache, numeroItems));
+                    }
+                });
+        });
+}
